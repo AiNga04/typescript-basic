6d5593fe8abc38b19a1163660b2532bdf2adbe18
AiNga04 <aingaspkt@gmail.com>
2025-03-03 00:52:29 +0700

    add build source base

---
+++ main.js
@@ -0,0 +1,7 @@
+var message = "Hello, World!";
+console.log(message);
+var name = "Zyna Dev";
+function welcome(name) {
+    return "Hello, ".concat(name);
+}
+console.log(welcome(name));
---
+++ main.ts
@@ -0,0 +1,10 @@
+const message: string = "Hello, World!";
+console.log(message);
+
+const name = "Zyna Dev";
+
+function welcome(name: string): string {
+  return `Hello, ${name}`;
+}
+
+console.log(welcome(name));
