672a28b4d2aed0cce14194e3c3d6f9be98adfb02
AiNga04 <aingaspkt@gmail.com>
2025-03-03 12:01:00 +0700

    done typescript-basic

--- main.js
+++ main.js
@@ -1,7 +1,329 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var message = "Hello, World!";
 console.log(message);
-var name = "Zyna Dev";
-function welcome(name) {
-    return "Hello, ".concat(name);
+var userName = "Zyna Dev";
+function welcome(userName) {
+    return "Hello, ".concat(userName);
 }
-console.log(welcome(name));
+console.log(welcome(userName));
+console.log(userName.toUpperCase());
+console.log(welcome(userName));
+var numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function (number) {
+    console.log(number * 2);
+});
+// Number
+var num = 3;
+console.log("number: " + num);
+// String
+var str = "Hello, Zyna!";
+console.log(str);
+// Boolean
+var isTrue = true;
+if (isTrue) {
+    console.log("isTrue: ".concat(isTrue));
+}
+// Object
+var user = {
+    name: "Zyna Dev",
+    age: 21,
+};
+console.log(user);
+// Array
+var array = [1, 2, 3, 4, 5];
+console.log(array);
+var skills = ["Zyna", 21];
+skills.push("dev");
+console.log(skills);
+// Tuple
+var skill2 = ["Nga", 21];
+console.log(skill2);
+var skill3 = ["Hello"];
+console.log(skill3);
+// Enum
+var API_STATUS;
+(function (API_STATUS) {
+    API_STATUS["PENDING"] = "PENDING";
+    API_STATUS["FULFILLING"] = "FULFILLING";
+    API_STATUS["REJECTED"] = "REJECTED";
+})(API_STATUS || (API_STATUS = {}));
+var pending = API_STATUS.PENDING;
+console.log(pending);
+var API;
+(function (API) {
+    API[API["PENDING"] = 0] = "PENDING";
+    API[API["FULFILLING"] = 1] = "FULFILLING";
+    API[API["REJECTED"] = 2] = "REJECTED";
+})(API || (API = {}));
+var pending2 = API.PENDING;
+console.log(pending2);
+// Any Type
+var anyType = "any type";
+console.log("Any: ".concat(anyType));
+anyType = true;
+console.log("Any: ".concat(anyType));
+// Void Type
+var sum = function (a, b) { return a + b; };
+console.log(sum(2, 6));
+var hello = function (message) {
+    console.log("Hello, ".concat(message));
+};
+hello("Nga");
+// Never
+function handleError(errorMessage) {
+    throw new Error(errorMessage);
+}
+// Union type
+function sumNumberOrString(a, b) {
+    if (typeof a === "number" && typeof b === "number")
+        return a + b;
+    else if (typeof a === "string" && typeof b === "string")
+        return a.concat(b);
+    return "Invalid parameters";
+}
+console.log("SumNum: ".concat(sumNumberOrString(2, 6)));
+console.log("SumString: ".concat(sumNumberOrString("Zyna", "dev")));
+console.log("SumError: ".concat(sumNumberOrString("Nga", 21)));
+function sumNumberOrStringUnion(a, b) {
+    if (typeof a === "number" && typeof b === "number")
+        return a + b;
+    else if (typeof a === "string" && typeof b === "string")
+        return a.concat(b);
+    return "Invalid parameters";
+}
+console.log("SumNum: ".concat(sumNumberOrStringUnion(2, 6)));
+console.log("SumString: ".concat(sumNumberOrStringUnion("Zyna", "dev")));
+console.log("SumError: ".concat(sumNumberOrStringUnion("Nga", 21)));
+//Default Parameters
+function hi(hi) {
+    if (hi === void 0) { hi = 'Hi Nga'; }
+    console.log(hi);
+}
+console.log(hi("Hi Zyna"));
+console.log(hi());
+var createArray = function () {
+    var spread = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        spread[_i] = arguments[_i];
+    }
+    return spread;
+};
+var myArray = createArray(1, 2, 3, 4, 5);
+console.log(myArray);
+var arr0 = myArray[0], rest = myArray.slice(1);
+console.log(arr0);
+console.log(rest);
+function addNew(a, b) {
+    return a + b;
+}
+console.log("addNum: ".concat(addNew(1, 4)));
+console.log("addString: ".concat(addNew('Hello', ' Zyna')));
+// Class
+var Card = /** @class */ (function () {
+    function Card(name, price, productionDate) {
+        this.name = name;
+        this.price = price;
+        this.productionDate = productionDate;
+    }
+    Card.prototype.getName = function () {
+        return this.name;
+    };
+    Card.prototype.getPrice = function () {
+        return this.price;
+    };
+    Card.prototype.getProductionDate = function () {
+        return this.productionDate;
+    };
+    Card.prototype.setName = function (name) {
+        this.name = name;
+    };
+    Card.prototype.setPrice = function (price) {
+        if (price < 0) {
+            throw new Error("price must be greater than 0");
+        }
+        this.price = price;
+    };
+    Card.prototype.getDetails = function () {
+        return "Card: ".concat(this.name, ", Price: $").concat(this.price, ", Production Date: ").concat(this.productionDate);
+    };
+    return Card;
+}());
+var itachi = new Card("Itachi", 50000, new Date());
+console.log(itachi.getName());
+itachi.setName("Itachi Luxury");
+console.log(itachi.getDetails());
+// Class TS
+var Car = /** @class */ (function () {
+    function Car(name, price, productionDate) {
+        this._name = name;
+        this._price = price;
+        this._productionDate = productionDate;
+    }
+    Object.defineProperty(Car.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (_name) {
+            this._name = _name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "price", {
+        get: function () {
+            return this._price;
+        },
+        set: function (_price) {
+            if (_price < 0) {
+                throw new Error("Price must be greater than 0");
+            }
+            this._price = _price;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "productionDate", {
+        get: function () {
+            return this._productionDate;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "detail", {
+        get: function () {
+            return "\n    Car: ".concat(this._name, "\n    Price: ").concat(this._price, "\n    Production Date: ").concat(this._productionDate);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Car;
+}());
+var toyota = new Car("Toyota", 50000, new Date());
+toyota.name = "Toyota Camry";
+console.log(toyota.detail);
+//Inheritance (Kế thừa)
+var Mazda = /** @class */ (function (_super) {
+    __extends(Mazda, _super);
+    function Mazda(name, price, productionDate, color) {
+        var _this = _super.call(this, name, price, productionDate) || this;
+        _this._color = color;
+        return _this;
+    }
+    Object.defineProperty(Mazda.prototype, "color", {
+        get: function () {
+            return this._color;
+        },
+        //Overwrite
+        set: function (color) {
+            this._color = color;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Mazda.prototype, "detail", {
+        get: function () {
+            return "\n    Car: ".concat(this.name, "\n    Color: ").concat(this._color, "\n    Price: ").concat(this.price, "\n    Production Date: ").concat(this.productionDate);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Mazda;
+}(Car));
+var mazda = new Mazda("Mazda", 60000, new Date(), "Red");
+console.log(mazda.detail);
+//Static
+var Circle = /** @class */ (function () {
+    function Circle() {
+        this.num = 100;
+    }
+    Circle.calculateArea = function (radius) {
+        return this.pi * radius * radius;
+    };
+    Circle.pi = 3.14;
+    return Circle;
+}());
+var num1 = new Circle();
+console.log(num1.num);
+console.log(Circle.calculateArea(10));
+// Abstract Classes
+var Employee = /** @class */ (function () {
+    function Employee(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._firstName + " " + this._lastName;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Employee.prototype.compensationStatement = function () {
+        return "".concat(this.fullName, " makes ").concat(this.getSalary(), " a month.");
+    };
+    return Employee;
+}());
+var EmployeeIT = /** @class */ (function (_super) {
+    __extends(EmployeeIT, _super);
+    function EmployeeIT(firstName, lastName, salary) {
+        var _this = _super.call(this, firstName, lastName) || this;
+        _this.salary = salary;
+        return _this;
+    }
+    EmployeeIT.prototype.getSalary = function () {
+        return this.salary;
+    };
+    return EmployeeIT;
+}(Employee));
+var EmployeeIOT = /** @class */ (function (_super) {
+    __extends(EmployeeIOT, _super);
+    function EmployeeIOT(firstName, lastName, rate, hours) {
+        var _this = _super.call(this, firstName, lastName) || this;
+        _this.rate = rate;
+        _this.hours = hours;
+        return _this;
+    }
+    EmployeeIOT.prototype.getSalary = function () {
+        return this.rate * this.hours;
+    };
+    return EmployeeIOT;
+}(Employee));
+var e1 = new EmployeeIT('Zyna', 'Dev', 1500);
+var e2 = new EmployeeIOT('Zyna', 'Dev', 10, 120);
+console.log(e1.compensationStatement());
+console.log(e2.compensationStatement());
+function getFullName(person) {
+    return "".concat(person.firstName, " ").concat(person.lastName);
+}
+var person = {
+    firstName: 'Zyna',
+    lastName: 'Dev',
+};
+console.log(getFullName(person));
+var User = /** @class */ (function () {
+    function User(name, age, salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+    User.prototype.getSalary = function (salary) {
+        return this.salary;
+    };
+    return User;
+}());
+var zyna = new User('Zyna', 21, 1000);
+console.log(zyna);
--- main.ts
+++ main.ts
@@ -1,10 +1,382 @@
 const message: string = "Hello, World!";
 console.log(message);
 
-const name = "Zyna Dev";
+const userName: string = "Zyna Dev";
 
-function welcome(name: string): string {
-  return `Hello, ${name}`;
+function welcome(userName: string): string {
+  return `Hello, ${userName}`;
 }
 
-console.log(welcome(name));
+console.log(welcome(userName));
+console.log(userName.toUpperCase());
+console.log(welcome(userName));
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+numbers.forEach((number) => {
+  console.log(number * 2);
+});
+
+// Number
+let num: number = 3;
+console.log("number: " + num);
+
+// String
+let str: string = "Hello, Zyna!";
+console.log(str);
+
+// Boolean
+let isTrue: boolean = true;
+if (isTrue) {
+  console.log(`isTrue: ${isTrue}`);
+}
+
+// Object
+const user: {
+  name: string;
+  age: number;
+} = {
+  name: "Zyna Dev",
+  age: 21,
+};
+
+console.log(user);
+
+// Array
+const array: number[] = [1, 2, 3, 4, 5];
+console.log(array);
+
+const skills: (string | number)[] = ["Zyna", 21];
+skills.push("dev");
+console.log(skills);
+
+// Tuple
+const skill2: readonly [string, number] = ["Nga", 21];
+console.log(skill2);
+
+const skill3: [string, number?] = ["Hello"];
+console.log(skill3);
+
+// Enum
+enum API_STATUS {
+  PENDING = "PENDING",
+  FULFILLING = "FULFILLING",
+  REJECTED = "REJECTED",
+}
+
+let pending = API_STATUS.PENDING;
+console.log(pending);
+
+enum API {
+  PENDING,
+  FULFILLING,
+  REJECTED,
+}
+
+let pending2 = API.PENDING;
+console.log(pending2);
+
+// Any Type
+let anyType: any = "any type";
+console.log(`Any: ${anyType}`);
+
+anyType = true;
+console.log(`Any: ${anyType}`);
+
+// Void Type
+const sum = (a: number, b: number): number => a + b;
+console.log(sum(2, 6));
+
+const hello = (message: string): void => {
+  console.log(`Hello, ${message}`);
+};
+hello("Nga");
+
+// Never
+function handleError(errorMessage: string): never {
+  throw new Error(errorMessage);
+}
+
+// Union type
+function sumNumberOrString(a: any, b: any): number | string {
+  if (typeof a === "number" && typeof b === "number") return a + b;
+  else if (typeof a === "string" && typeof b === "string") return a.concat(b);
+  return "Invalid parameters";
+}
+
+console.log(`SumNum: ${sumNumberOrString(2, 6)}`);
+console.log(`SumString: ${sumNumberOrString("Zyna", "dev")}`);
+console.log(`SumError: ${sumNumberOrString("Nga", 21)}`);
+
+type numberString = number | string;
+
+function sumNumberOrStringUnion(a: numberString, b: numberString): numberString {
+  if (typeof a === "number" && typeof b === "number") return a + b;
+  else if (typeof a === "string" && typeof b === "string") return a.concat(b);
+  return "Invalid parameters";
+}
+
+console.log(`SumNum: ${sumNumberOrStringUnion(2, 6)}`);
+console.log(`SumString: ${sumNumberOrStringUnion("Zyna", "dev")}`);
+console.log(`SumError: ${sumNumberOrStringUnion("Nga", 21)}`);
+
+//Default Parameters
+function hi(hi = 'Hi Nga') {
+  console.log(hi);
+}
+
+console.log(hi("Hi Zyna"));
+console.log(hi());
+
+const createArray = (...spread: number[]): number[] => {
+  return spread;
+};
+
+let myArray: number[] = createArray(1, 2, 3, 4, 5);
+
+console.log(myArray);
+
+const [arr0, ...rest] = myArray;
+
+console.log(arr0);
+console.log(rest);
+
+// Overloading declarations
+function addNew(a: number, b: number): number;
+function addNew(a: string, b: string): string;
+
+function addNew(a: any, b: any): any {
+  return a + b;
+}
+
+console.log(`addNum: ${addNew(1, 4)}`);
+console.log(`addString: ${addNew('Hello', ' Zyna')}`);
+
+// Class
+class Card {
+  private name: string;
+  private price: number;
+  readonly productionDate: Date;
+
+  constructor(name: string, price: number, productionDate: Date) {
+    this.name = name;
+    this.price = price;
+    this.productionDate = productionDate;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getPrice(): number {
+    return this.price;
+  }
+  getProductionDate(): Date {
+    return this.productionDate;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  setPrice(price: number): void {
+    if(price < 0){
+      throw new Error("price must be greater than 0");
+    }
+    this.price = price;
+  }
+
+  getDetails(): string {
+    return `Card: ${this.name}, Price: $${this.price}, Production Date: ${this.productionDate}`;
+  }
+}
+
+let itachi = new Card("Itachi", 50000, new Date());
+console.log(itachi.getName());
+
+itachi.setName("Itachi Luxury");
+console.log(itachi.getDetails());
+
+// Class TS
+class Car {
+  private _name: string;
+  private _price: number;
+  readonly _productionDate: Date;
+
+  constructor(name: string, price: number, productionDate: Date) {
+    this._name = name;
+    this._price = price;
+    this._productionDate = productionDate;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  get productionDate(): Date {
+    return this._productionDate;
+  }
+
+  set name(_name: string) {
+    this._name = _name;
+  }
+
+  set price(_price: number) {
+    if (_price < 0) {
+      throw new Error("Price must be greater than 0");
+    }
+    this._price = _price;
+  }
+
+  get detail(): string {
+    return `
+    Car: ${this._name}
+    Price: ${this._price}
+    Production Date: ${this._productionDate}`;
+  }
+}
+
+let toyota = new Car("Toyota", 50000, new Date());
+
+toyota.name = "Toyota Camry";
+
+console.log(toyota.detail);
+
+//Inheritance (Kế thừa)
+class Mazda extends Car {
+  private _color: string;
+  constructor(name: string, price: number, productionDate: Date, color: string) {
+    super(name, price, productionDate);
+    this._color = color;
+  }
+
+  //Overwrite
+  set color(color: string) {
+    this._color = color;
+  }
+
+  get color(): string {
+      return this._color;
+  }
+
+  get detail(): string {
+    return `
+    Car: ${this.name}
+    Color: ${this._color}
+    Price: ${this.price}
+    Production Date: ${this.productionDate}`;
+  }
+}
+
+let mazda = new Mazda("Mazda", 60000, new Date(), "Red");
+console.log(mazda.detail);
+
+//Static
+class Circle {
+  static pi: number = 3.14;
+  public num: number = 100;
+
+  static calculateArea(radius: number) {
+    return this.pi * radius * radius;
+  }
+}
+
+let num1 = new Circle();
+console.log(num1.num);
+console.log(Circle.calculateArea(10));
+
+// Abstract Classes
+abstract class Employee {
+  private _firstName: string;
+  private _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  abstract getSalary(): number;
+
+  get fullName(): string {
+    return this._firstName + " " + this._lastName;
+  }
+
+  compensationStatement(): string {
+    return `${this.fullName} makes ${this.getSalary()} a month.`;
+  }
+}
+
+class EmployeeIT extends Employee {
+  constructor(firstName: string, lastName: string, private salary: number) {
+    super(firstName, lastName);
+  }
+
+  getSalary(): number {
+    return this.salary;
+  }
+}
+
+class EmployeeIOT extends Employee {
+  constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
+    super(firstName, lastName);
+  }
+
+  getSalary(): number {
+    return this.rate * this.hours;
+  }
+}
+
+let e1 = new EmployeeIT('Zyna', 'Dev', 1500);
+let e2 = new EmployeeIOT('Zyna', 'Dev', 10, 120);
+
+console.log(e1.compensationStatement());
+console.log(e2.compensationStatement());
+
+//Interface
+interface IPerson{
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
+
+function getFullName(person): string {
+  return `${person.firstName} ${person.lastName}`;
+}
+
+let person: IPerson = {
+  firstName: 'Zyna',
+  lastName: 'Dev',
+}
+
+console.log(getFullName(person));
+
+interface IUser{
+  name: string;
+  age: number;
+  salary: number;
+
+  getSalary:(salary: number) => number;
+}
+
+class User implements IUser {
+  name: string;
+  age: number;
+  salary: number;
+
+  constructor(name: string, age: number, salary: number) {
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+  }
+
+  getSalary(salary: number): number {
+    return this.salary;
+  }
+}
+
+let zyna = new User('Zyna', 21, 1000);
+
+console.log(zyna);
